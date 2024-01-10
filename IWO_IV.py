5b10ec54e1ea80e8fc102bcd6d07927073a97b89
Gyujun Jeong <kyur1ter1a@gmail.com>
2024-01-09 21:41:53 -0500

    Leaky ReLU Apply

--- IWO_IV.py
+++ IWO_IV.py
@@ -140,26 +140,28 @@ class MLP(torch.nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
         self.fc1 = torch.nn.Linear(3, 25)
-        self.fc2 = torch.nn.Linear(25, 1)
+        self.fc2 = torch.nn.Linear(25, 12)
+        self.fc3 = torch.nn.Linear(12, 1)
         self.dropout = torch.nn.Dropout(0.5)
+        self.tanh = torch.nn.Tanh()
         self.relu = torch.nn.ReLU()
         self.leaky_relu = torch.nn.LeakyReLU(0.01)
         self.bn1 = torch.nn.BatchNorm1d(25)
-        self.bn2 = torch.nn.BatchNorm1d(1)
+        self.bn2 = torch.nn.BatchNorm1d(12)
+        self.bn3 = torch.nn.BatchNorm1d(1)
     
     def forward(self, x):
         x = self.fc1(x)
-        x = self.bn1(x)
+        #x = self.bn1(x)
         x = self.leaky_relu(x)
         #x = self.dropout(x)
         x = self.fc2(x)
-        x = self.bn2(x)
+        #x = self.bn2(x)
         x = self.leaky_relu(x)
         #x = self.dropout(x)
-        #x = self.fc3(x)
+        x = self.fc3(x)
         #x = self.bn3(x)
-        #x = self.leaky_relu(x)
-        #x = self.dropout(x)
+        #x = self.tanh(x)
         return x
 
 # Create an instance of the MLP class
@@ -167,18 +169,18 @@ model = MLP()
 
 torch.nn.init.xavier_uniform(model.fc1.weight)
 torch.nn.init.xavier_uniform(model.fc2.weight)
-# torch.nn.init.xavier_uniform(model.fc3.weight)
+torch.nn.init.xavier_uniform(model.fc3.weight)
 # torch.nn.init.xavier_uniform(model.fc4.weight)
 
 loss_function = nn.MSELoss()
-#optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+#optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5)
 
 # losses = []
 # criterion = nn.MSELoss() # <== 파이토치에서 제공하는 평균 제곱 오차 함수\
 
-nb_epochs = 2500
+nb_epochs = 3000
 MLoss = [] 
 for epoch in range(0, nb_epochs):
      
@@ -220,7 +222,7 @@ for epoch in range(0, nb_epochs):
 
 #print('Loss (epoch: %4d): %.8f' %(epoch+1, mean_loss))
 # Print the loss only every 10 epochs
-    if (epoch + 1) % 50 == 0:
+    if (epoch + 1) % 10 == 0:
         print('Loss (epoch: %4d): %.8f' % (epoch + 1, mean_loss))
     current_loss = 0.0
     MLoss.append(mean_loss)
